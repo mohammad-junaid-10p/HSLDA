7996a266750261bb590a0b779eb4b4016fe3796e
mohammad.junaid <mohammad.junaid@10pearls.com>
2021-03-30 11:40:55 +0500

    ui: all view payment screen completed

--- assets/dist/script.js
+++ assets/dist/script.js
@@ -1,6 +1,12 @@
 let toggleBtn = document.querySelector('#toggle');
 let layoutWrap = document.querySelector('.layout-wrapper');
-
+let accordianBtn = document.querySelector('.hslda-card-footer');
+let accordian = document.querySelector('.card-accordian-wrapper');
 toggleBtn.onclick = function () { 
     layoutWrap.classList.toggle('sidebar-expanded');
 };
+
+function accordianToggle(element) {
+    element.classList.toggle('accordian-expanded');
+};
+
