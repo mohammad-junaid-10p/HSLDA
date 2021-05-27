b5669444ea6c466123d2e007571dfffb979a842e
mohammad.junaid <mohammad.junaid@10pearls.com>
2021-05-27 11:26:24 +0500

    multiple changes

--- assets/dist/script.js
+++ assets/dist/script.js
@@ -1,7 +1,9 @@
-let toggleBtn = document.querySelector('#toggle');
-let layoutWrap = document.querySelector('.layout-wrapper');
-let accordianBtn = document.querySelector('.hslda-card-footer');
-let accordian = document.querySelector('.card-accordian-wrapper');
+window.onload = toggleBtn = document.querySelector('#toggle');
+window.onload = layoutWrap = document.querySelector('.layout-wrapper');
+window.onload = accordianBtn = document.querySelector('.hslda-card-footer');
+window.onload = accordian = document.querySelector('.card-accordian-wrapper');
+
+
 toggleBtn.onclick = function () { 
     layoutWrap.classList.toggle('sidebar-expanded');
 };
