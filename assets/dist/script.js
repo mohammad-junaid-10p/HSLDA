01f5cfe74803b473c74861e31501abec0e812997
mohammad.junaid <mohammad.junaid@10pearls.com>
2021-06-29 14:41:38 +0500

    change password added

--- assets/dist/script.js
+++ assets/dist/script.js
@@ -1,13 +1,23 @@
-window.onload = toggleBtn = document.querySelector('#toggle');
-window.onload = layoutWrap = document.querySelector('.layout-wrapper');
-window.onload = accordianBtn = document.querySelector('.hslda-card-footer');
-window.onload = accordian = document.querySelector('.card-accordian-wrapper');
+let accordianBtn = document.querySelector('.hslda-card-footer');
+let accordian = document.querySelector('.card-accordian-wrapper');
 
 
-toggleBtn.onclick = function () { 
+function sidebarToggle() { 
+    layoutWrap = document.querySelector('.layout-wrapper');
     layoutWrap.classList.toggle('sidebar-expanded');
 };
 
 function accordianToggle(element) {
     element.classList.toggle('accordian-expanded');
 };
+
+function accordianToggle(element) {
+    element.classList.toggle('accordian-expanded');
+};
+
+function eyeToggle(element) {
+    element.classList.toggle('icon-eye');
+    let password = element.previousElementSibling;
+    let type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+}
